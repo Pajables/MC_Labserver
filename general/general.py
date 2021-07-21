2a1ddf86266832aba0fcee03feaab94612528ffb
Pajables <mattnel@outlook.com>
2021-07-21 14:45:22 +0200

    Minor changes

--- general/general.py
+++ general/general.py
@@ -1,10 +1,13 @@
 # import db_retrieve
 from flask import Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
+from .. import db
 
 # file to hold general pages not requiring a login.
 
 general = Blueprint('general', __name__)
+connection = db.engine.raw_connection()
+cursor = connection.cursor()
 
 
 @general.route('/')
@@ -16,8 +19,8 @@ def index():
 @login_required
 def robots_display():
     # Get robots from database and display in a table
-    mycursor.execute("SELECT * FROM ROBOTS")
-    robots_data = mycursor.fetchall()
+    cursor.execute("SELECT * FROM ROBOTS")
+    robots_data = cursor.fetchall()
 
     if request.method == "GET":
         return render_template('robots_display.html', robots_data=robots_data)
@@ -28,23 +31,24 @@ def robots_display():
 @login_required
 def reactions_display():
     # Get reactions from database and display in a table
-    mycursor.execute("SELECT * FROM REACTION_STATUS")
-    reactions_data = mycursor.fetchall()
+    cursor.execute("SELECT * FROM REACTION_STATUS")
+    reactions_data = cursor.fetchall()
 
-    if request.method = 'GET':
+    if request.method == 'GET':
         # todo create a reactions.html file and code for the arrangement of styles for the main page(__init__.py)
         return render_template('reactions.html', reactions_data=reactions_data)
         # render a template using reactions_list
         # show links to specific reaction tables
 
+
 @general.route("/reactions/display", methods=['GET', 'POST'])
 @login_required
 def reaction_table_display():
     # display only entries from specific reaction
-    mycursor.execute("SELECT * FROM REACTION_STATUS WHERE REACTION_NAME = "Aspirin Synthesis" AND REACTION_STATUS ="COMPLETE"")
-    reaction_data = mycursor.fetchall()
+    cursor.execute("SELECT * FROM REACTION_STATUS WHERE REACTION_NAME = 'Aspirin Synthesis' AND REACTION_STATUS ='COMPLETE'")
+    reaction_data = cursor.fetchall()
 
-    if request.method = 'POST':
+    if request.method == 'POST':
         # todo create a reactions/display.html file and code for the arrangement of styles for the main page(__init__.py)
         return render_template('reactions/display', reaction_data=reaction_data)
 
