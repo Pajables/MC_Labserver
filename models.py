6e4b9a85f813e6bd82be47f16d2e555b8985cd49
Amykelani <77246887+Amykelani@users.noreply.github.com>
2021-07-27 10:00:58 +0200

    Major changes on model.py

--- models.py
+++ models.py
@@ -4,38 +4,29 @@ from . import db
 # each table in the database needs a class to be created for it
 # db.model is required - don't change it
 # identify all columns by name and data type
-class User(UserMixin, db.model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
 
-class Robot_Status(db.model):
-    robot_id = db.Column(db.String(100), primary_key=True)
-    robot_name = db.Column(db.String(100), unique=True)
-    ip_address = db.Column(db.String(100))
-    robot_status = db.column(db.String(100))
-    current_job = db.column(db.String(100))
-    last_update_date = db.Column(db.String(100))
 
-class Reaction_Status(db.model):
-    robot_id = db.Column(db.String(100), primary_key=True)
-    robot_name = db.Column(db.String(100), unique=True)
-    reaction_name = db.Column(db.String(100))
-    reaction_status = db.Column(db.String(100))
-    last_update_date = db.Column(db.String(100))
-    job_completion_date = db.Column(db.String(100))
+class User(UserMixin, db.Model):
+    __table__ = db.Model.metadata.tables['Users']
 
-class Reaction_Parameters(db.model):
-    reaction_name = db.Column(db.String(100))
-    robot_id = db.Column(db.String(100), primary_key=True)
-    robot_name = db.Column(db.String(100), unique=True)
-    parm1 = db.Column(db.String(100))
-    parm1_value = db.column(db.String(100))
-    last_update_date = db.Column(db.String(100))
+    def get_id(self):
+        return self.USER_ID
 
-class UJ_Robots_Communication(db.model):
-    robot_id = db.Column(db.String(100), primary_key=True)
-    robot_name = db.Column(db.String(100), unique=True)
-    status = db.Column(db.String(100))
-    execute = db.Column(db.Integer)
-    last_update_date = db.Column(db.String(100))
+class RobotStatus(db.Model):
+    __table__ = db.Model.metadata.tables['Robot_Status']
+
+
+class ReactionStatus(db.Model):
+    __table__ = db.Model.metadata.tables['Reaction_Status']
+
+
+class ReactionParameters(db.Model):
+    __table__ = db.Model.metadata.tables['Reaction_Parameters']
+
+
+class UJRobotsCommunication(db.Model):
+    __table__ = db.Model.metadata.tables['UJ_Robots_Communication']
+
+# robots model for connection and authentication
+class Robots(db.model):
+    __table__ = db.Model.metadata.tables['Robots']
